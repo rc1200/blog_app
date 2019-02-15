59a15dc375f129e0ad7e9a907e1b13d83c6a58af
rc1200 <ron.calibuso@gmail.com>
2019-02-15 00:52:33 -0500

    add feature to edit blog post

--- blog_app/blog/urls.py
+++ blog_app/blog/urls.py
@@ -4,6 +4,6 @@ urlpatterns = [
     # 127.0.0.1:8000 so look at the views.post_list view and render that.
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    re_path(r'^post/(new|edit)/$', views.post_new, name='post_new'),
-
+    re_path(r'^post/(new|newpost)/$', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
